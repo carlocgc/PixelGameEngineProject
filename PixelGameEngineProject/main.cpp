0a2b04d830788029038ec6b172be01577a396d31
Carlo Mongiello <carlocgc1@gmail.com>
2021-01-30 17:09:38 +0000

    Added bounce and friction

--- PixelGameEngineProject/main.cpp
+++ PixelGameEngineProject/main.cpp
@@ -1,6 +1,12 @@
 #define OLC_PGE_APPLICATION
 #include "olcPixelGameEngine.h"
 
+/**
+ * \brief Print with cout
+ * \param value Value to print
+ */
+#define LOG(value) std::cout << (value) << std::endl
+
 struct VertletPoint
 {
 	float x;
@@ -13,7 +19,7 @@ struct VertletPoint
 		y(_y),
 		oldx(_oldx),
 		oldy(_oldy)
-	{}	
+	{}
 };
 
 // Override base class with your custom functionality
@@ -30,7 +36,7 @@ public:
 	{
 		// Called once at the start, so create things here
 
-		m_points.emplace_back(VertletPoint(100, 100, 95, 95));
+		m_Points.emplace_back(VertletPoint(100, 100, 95, 95));
 
 
 		return true;
@@ -39,57 +45,69 @@ public:
 	bool OnUserUpdate(float fElapsedTime) override
 	{
 		Clear(olc::DARK_BLUE);
-		
-		UpdatePoints(m_points, fElapsedTime);
-		
-		RenderPoints(m_points);
-			
+
+		UpdatePoints(m_Points, fElapsedTime);
+
+		RenderPoints(m_Points);
+
 		return true;
 	}
 
 
 private:
-
-	const float m_PointSize = 10.f;
 	
-	std::vector<VertletPoint> m_points;
+	const float m_PointRadius = 5.f;
+	
+	const float m_Bounce = 0.9f;
+	const float m_Gravity = 0.5f;
+	const float m_Friction = 0.999f;
 
-	void UpdatePoints(std::vector<VertletPoint>& points, float fElapsedTime)
+	std::vector<VertletPoint> m_Points;
+
+	void UpdatePoints(std::vector<VertletPoint>& points, const float fElapsedTime) const
 	{
+		// TODO incorporate fElapsedTime
+
 		for (auto& p : points)
 		{
 			// calc velocity
-			const float vx = p.x - p.oldx;
-			const float vy = p.y - p.oldy;
+			float vx = (p.x - p.oldx) * m_Friction;
+			float vy = (p.y - p.oldy) * m_Friction;
+
+			// update old pos for next frame
+			p.oldx = p.x;
+			p.oldy = p.y;			
 			
 			// apply velocity
-			p.x += vx * fElapsedTime;
-			p.y += vy * fElapsedTime;
-			
+			p.x += vx;
+			p.y += vy;
+			// apply gravity
+			p.y += m_Gravity;
+
 			// confine x to screen bounds
-			if (p.x >= ScreenWidth())
+			if (p.x >= ScreenWidth() - m_PointRadius)
 			{
-				p.x = ScreenWidth();
-				// invert x velocity
-				p.oldx = p.x + vx;		
+				p.x = ScreenWidth() - m_PointRadius;
+				// invert x velocity, apply bounce speed reduction
+				p.oldx = p.x + vx * m_Bounce;
 			}
-			else if (p.x  < 0)
+			else if (p.x < 0 + m_PointRadius)
 			{
-				p.x = 0;
-				p.oldx = p.x + vx;
+				p.x = 0 + m_PointRadius;
+				p.oldx = p.x + vx * m_Bounce;
 			}
 
 			// confine y to screen bounds
-			if (p.y >= ScreenHeight())
+			if (p.y >= ScreenHeight() - m_PointRadius)
 			{
-				p.y = ScreenHeight();
-				// invert y velocity
-				p.oldy = p.y + vy;
+				p.y = ScreenHeight() - m_PointRadius;
+				// invert y velocity, apply bounce speed reduction
+				p.oldy = p.y + vy * m_Bounce;
 			}
-			else if (p.y < 0)
+			else if (p.y < 0 + m_PointRadius)
 			{
-				p.y = 0;
-				p.oldy = p.y + vx;
+				p.y = 0 + m_PointRadius;
+				p.oldy = p.y + vy * m_Bounce;
 			}
 		}
 	}
@@ -98,7 +116,7 @@ private:
 	{
 		for (auto& p : points)
 		{
-			DrawRect(p.x, p.y, m_PointSize, m_PointSize);
+			FillCircle(p.x, p.y, m_PointRadius, olc::WHITE);			
 		}
 	}
 };
